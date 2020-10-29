d1510e08123eda132fee2f0beadb38aa60269318
MingZheng668 <mzheng22@my.centennialcollege.ca>
2020-10-28 23:19:43 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://mingzheng:bpMZgzouuIxwGkQJ@mongodbserver.3q3hc.mongodb.net/book_store?retryWrites=true&w=majority"
+  //"mongodb://localhost/books229"
+};
